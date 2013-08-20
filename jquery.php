ae23cbbc0ab6aefb508d803f66d6fc08f92c19ad
Fedor <vlasenkofedor@gmail.com>
2013-08-20 15:59:04 +0300

    Create jquery.php

---
+++ jquery.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.Joomla_Options
+ * @autor 		Fedor Vlasenko
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// Запрет прямого доступа.
+defined('_JEXEC') or die;
+abstract class JHtmlJquery
+{
+    public static function __callStatic($_name, $_param){}
+}
