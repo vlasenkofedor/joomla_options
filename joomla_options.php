320fbed420e47a133eb2d97e79f61cb65c59632b
Fedor <vlasenkofedor@gmail.com>
2013-10-27 02:51:20 +0300

    Update joomla_options.php

--- joomla_options.php
+++ joomla_options.php
@@ -11,18 +11,20 @@ defined('_JEXEC') or die;
 
 class plgSystemJoomla_Options extends JPlugin
 {
+	
 	public function onAfterInitialise()
 	{
 		// Если мы в админке - выход.
-	if (JFactory::getApplication()->isAdmin()) return;
-
+		if (JFactory::getApplication()->isAdmin()) return;
+	
 		// Заменяем базовый класс JHtmlBehavior переопределенным
-	if ($this->params->get('disable_mootools', 0) =='1') include 'behavior.php';
-
-        // Заменяем базовый класс HtmlJquery переопределенным
-        if ($this->params->get('disable_jquery', 0) =='1') include 'jquery.php';
-
-        // Заменяем базовый класс JHtmlBootstrap переопределенным
-        if ($this->params->get('disable_bootstrap', 0) =='1') include 'bootstrap.php';
+		if ($this->params->get('disable_mootools', 0) =='1') include 'behavior.php';
+	
+	        // Заменяем базовый класс HtmlJquery переопределенным
+	        if ($this->params->get('disable_jquery', 0) =='1') include 'jquery.php';
+	
+	        // Заменяем базовый класс JHtmlBootstrap переопределенным
+	        if ($this->params->get('disable_bootstrap', 0) =='1') include 'bootstrap.php';
 	}
+	
 }	
