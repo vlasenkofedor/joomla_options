9da9b5592c7ed8a8ec2a9cc36b847188523160eb
poznakomlus <vlasenkofedor@mail.ru>
2014-04-19 11:51:02 +0300

    Test me

--- behavior.php
+++ behavior.php
@@ -2,13 +2,16 @@
 /**
  * @package     Joomla.Plugin
  * @subpackage  System.Joomla_Options
- * @autor 		Fedor Vlasenko
+ * @autor        Fedor Vlasenko
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 // Запрет прямого доступа.
 defined('_JEXEC') or die;
+
 abstract class JHtmlBehavior
 {
-	public static function __callStatic($_name, $_param){}
+	public static function __callStatic($_name, $_param)
+	{
+	}
 }
